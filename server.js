1e2e70cbd945cfc6c8076e15d6c5c69a3adb1cad
codevubuntu <codev.ubuntu@gmail.com>
2017-07-13 13:49:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
-app.get('/', function (req, res){
- res.send('Hello World!');
-} );
-var server = app.listen(server_port, server_ip_address, function(){
- var host = server.address().address;
- var port = server.address().port;
- console.log('Listening at http://%s:%s', host, port);
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello World');
 });
-module.exports = app ;
+server.listen(8080);
