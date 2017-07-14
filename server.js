80389b98bd5de0d1384b1c498eb4bbc27eb1bc85
codevubuntu <codev.ubuntu@gmail.com>
2017-07-14 19:14:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-res.send('Hello World');
+  res.sendFile(__dirname+"/view/index.html");
 })
 var server = app.listen(8080, function () {
-var host = server.address().address ;
-var port = server.address().port ;
-console.log("Example app listening at http://%s:%s", host, port) ;
+  var host = server.address().address ;
+  var port = server.address().port ;
+  console.log("Example app listening at http://%s:%s", host, port) ;
 })
