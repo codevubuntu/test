efb5058b9a4439ae11dcd8944ac627ced4b18cf1
codevubuntu <codev.ubuntu@gmail.com>
2017-07-14 20:22:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname+"/view/index.html");
 })
+.get('/services', function(req, res){
+  res.senFile(__dirname+"/view/services.html")
+})
+.get('/contact', function(req, res){
+  res.send(__dirname+"/view/contact.html");
+});
+
 var server = app.listen(8080, function () {
   var host = server.address().address ;
   var port = server.address().port ;
